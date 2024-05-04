81e73eb223cffe699eefcace7ab4bd08c8da6cf1
Higor Bueno da Silva <h.higorbsilva.dev@gmail.com>
2024-05-04 03:39:57 -0300

    feature: adding 'new' command to create a directory for the project

--- Readme.md
+++ Readme.md
@@ -9,7 +9,7 @@ polar new project_name --git --lang=c
 
 ## Features TO-DO:
 - [ ] Implement the ´new´ command
-  - [ ] Create a directory ´~/Projects/project_name´
+  - [X] Create a directory ´~/Projects/project_name´
   - [ ] If ´--git´ option, init git
   - [ ] For this first version, create the "hello world" for the version of '--lang' option
     - [ ] Implement a parser for this option that returns a symbol for given language
--- src/main.c
+++ src/main.c
@@ -1,20 +1,53 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+#define PJ_NAME_SIZE 20
+#define PATH_SIZE 30
+#define PROJECTS_PATH "/home/higor/Projects" // ALTERATE THIS TO YOUR PERSONAL PROJECT PATH
+
+void put_log(char *msg) {
+  fprintf(stdout, "%s", msg);
+}
+
+int create_project(const char *name, const char *lang, const bool git) {
+  char project_path[PATH_SIZE];
+  sprintf(project_path, "%s/%s", PROJECTS_PATH, name);
+
+  struct stat st = {0};
+  if(stat(project_path, &st) != -1) {
+    fprintf(stderr, "Project: '%s' Already exists!\n", name);
+    exit(-1);
+  }
+
+  mkdir(project_path, 0777);
+  put_log("[INFO]: Creating path.\n");
+
+  return 0;
+}
 
 /*
  * A Simple Hello World code
  */
 int main(int argc, char **argv) {
-  printf("Hello, Command Line! %d\n", argc);
-  
-  if(argc > 2) {
-    if(strcmp(argv[1], "new") == 0) {
-      char *name_file;
-      sprintf(name_file, "%s.txt", argv[2]);
-      FILE *file = fopen(name_file, "w");
-      fprintf(file, "Hello, %s!\n", argv[2]);
+  if(argc > 1) {
 
-      fclose(file);
+    // 'new' command
+    if(strcmp(argv[1], "new") == 0) {
+      if(argc >= 3) {
+
+        char project_name[PJ_NAME_SIZE];
+        strcpy(project_name, argv[2]);
+        create_project(project_name, NULL, false);
+
+      } else {
+        fprintf(stderr, "'polar new' command requires an project name.\nType 'polar new (project_name)'.\n");
+        exit(-1);
+      }
+
     }
 
   }
